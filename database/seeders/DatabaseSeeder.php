f7d284a6a9f2a2c51aff09f7305b1698d9f8d930
wiljean2001 <86029273+wiljean2001@users.noreply.github.com>
2021-11-23 16:49:59 -0500

    a

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SpiritSeeder::class);
         $this->call(TableSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
     }
 }

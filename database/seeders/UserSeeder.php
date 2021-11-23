a24752dce3e56804b74f1afc562a474774186b73
wiljean2001 <86029273+wiljean2001@users.noreply.github.com>
2021-11-23 16:34:03 -0500

    corrección de error de usuarios registrados

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(1)->create();
+        // assignRole('admin'),
+        User::create(
+            [
+                'name' => 'Ricardo Piglia',
+                'email' => 'root-admin_wiljean@piedrascalientes.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ])->assignRole('admin');
     }
 }

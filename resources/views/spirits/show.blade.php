65fa206786efbf4affc160f02ab43dd02a116c9a
Depjea <jeancitoz02@gmail.com>
2021-11-23 10:40:32 -0500

    Licores
    Todo referente a licores, editar, agregar y show

---
+++ resources/views/spirits/show.blade.php
@@ -0,0 +1,41 @@
+@extends('adminlte::page')
+
+@section('title', 'licor')
+
+@section('content')
+
+<div class="container-fluid w-auto">
+    <div class="row">
+        <h1 class="my-lg-2 my-2">MOSTRAR LICORES</h1>
+        {{-- <table class="table table-flush" id="datatable-buttons"> --}}
+            <x-adminlte-datatable id="table1" :heads="$heads" head-theme="dark" theme="light" :config='$config' with-buttons
+                with-footer>
+                @foreach($spirits as $row)
+                <tr>
+                    <td>{!! $row->id !!}</td>
+                    <td>{!! $row->name !!}</td>
+                    <td>S/ {!! $row->price !!}</td>
+                    <td>{!! $row->description !!}</td>
+                    <td>
+                        <img src="{!! asset('files/spirits/'.$row->image) !!}" alt="imagen-licor" class=" w-50">
+                    </td>
+                    <td>{!! $row->stock !!} uds.</td>
+                    <td>
+                        <button class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
+                            <i class="fa fa-lg fa-fw fa-pen"></i>
+                        </button>
+                        <button class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
+                            <i class="fa fa-lg fa-fw fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                @endforeach
+            </x-adminlte-datatable>
+    </div>
+</div>
+@stop
+@extends('layouts.footers.footer')
+
+
+@section('js')
+@stop
---
+++ resources/views/spirits/update.blade.php
@@ -0,0 +1,110 @@
+@extends('adminlte::page')
+
+@section('title', 'licor')
+
+@section('content')
+<div class="d-flex flex-row w-100 flex-wrap">
+    <div class="w-50">
+        <h1 class="my-lg-2 mt-3 mt-lg-3">ACTUALIZAR LICOR</h1>
+        <form action=" {{ route('spirit.update') }}" method="POST" class="d-flex flex-column"
+            enctype="multipart/form-data">
+            @csrf
+            <input type="text" name="id" id="id" hidden>
+            <div class="col-md-auto">
+                <label for="name">Ingresar licor</label>
+                <div class="form-group">
+                    <input type="text" class="form-control focused w-100" placeholder="Tequila" name="name"
+                        required id="name">
+                </div>
+            </div>
+            <div class="col-md-auto">
+                <label for="description">Ingresar descripción</label>
+                <div class="form-group">
+                    <textarea class="form-control" placeholder="Tipos de Tequila" name="description" required
+                        id="descript"></textarea>
+                </div>
+            </div>
+            <div class="col-md-auto">
+                <label for="price">Ingresar precio</label>
+                <div class="form-group">
+                    <input type="number" class="form-control" placeholder="35.00" name="price" required id="price"
+                        step="0.01">
+                </div>
+            </div>
+            <div class="col-md-auto col-lg-auto">
+                <label for="price">Ingresar stock ud.</label>
+                <div class="form-group">
+                    <input type="number" class="form-control" placeholder="150" name="stock" required id="stock">
+                </div>
+            </div>
+            <div class="col-md-auto col-lg-auto">
+                <label for="image">Ingresar imagen</label>
+                <div class="form-group">
+                    <div class="custom-file">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="customFileLang" lang="es" name="image"
+                                accept="image/*">
+                            <label class="custom-file-label" for="customFileLang">Seleccionar imagen</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-4 mb-lg-4 text-center">
+                <div class="col-md-auto mt-4 mt-lg-4 px-5 px-lg-5">
+                    <input type="submit" class="btn bg-gradient-purple w-75 py-2 py-lg-2" name="register"
+                        value="ACTUALIZAR LICOR">
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="p-2 p-lg-2 table-responsive w-50">
+        <x-adminlte-datatable id="table2" :heads="$heads" head-theme="dark" :config='$config' with-buttons with-footer
+            hoverable>
+            @foreach($spirits as $row)
+            <tr>
+                <td>{!! $row->id !!}</td>
+                <td>{!! $row->name !!}</td>
+                <td class="d-flex flex-row-reverse">
+                    {!! $row->price !!}
+                    <div>S/</div>
+                </td>
+                <td>{!! $row->description !!}</td>
+                <td>
+                    <img src="{!! asset('files/spirits/'.$row->image) !!}" alt="imagen-licor" class=" w-50">
+                </td>
+                <td>
+                    {!! $row->stock !!}
+                    <label for=""> uds.</label>
+                    {{-- <div> uds.</div> --}}
+                </td>
+                <td>
+                    <div>
+                        <x-adminlte-button label="Editar" theme="warning" icon="fas fa-pen-square" id="btn-edit" />
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </x-adminlte-datatable>
+    </div>
+</div>
+@stop
+@extends('layouts.footers.footer')
+
+
+@push('js')
+<script>
+    $('#table2').on('click', '#btn-edit', function (e) {
+        var table = $('#table2').DataTable();
+        e.preventDefault();
+        var row = $(this).parent().parent().parent()[0];
+        var data = table.row(row).data();
+        var price = parseFloat(data[2], 10);
+        $('#id').val(data[0]);
+        $('#name').val(data[1]);
+        $('#price').val(price).text().replace(',', '.');
+        $('#descript').val(data[3]);
+        $('#stock').val(parseInt(data[5]));
+        // console.log(data);
+});
+</script>
+@endpush
